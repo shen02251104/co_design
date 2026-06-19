ffb566397ce7cbc32b7132e324d1d14d026bc804
bbs001 <4452356313846505-bbs001@noreply.coze.cn>
2026-06-19 15:05:17 +0800

    fix: 取消注释启用分类接口getCategories加载分类列表

--- design-editor/src/components/modules/panel/wrap/components/searchHeader.vue
+++ design-editor/src/components/modules/panel/wrap/components/searchHeader.vue
@@ -66,13 +66,13 @@ const state = reactive<TState>({
 
 if (props.type != 'none') {
   state.materialCates = [{ id: 0, name: '示例模板' }]
-  // api.home.getCategories({ type: 1 }).then((list: any) => {
-  //   list.unshift({ id: 0, name: '全部' })
-  //   state.materialCates = list
-  //   const { cate } = route.query
-  //   cate && (state.currentIndex = cate as string)
-  //   cate && action('change', state.materialCates[Number(cate)], Number(cate))
-  // })
+  api.home.getCategories({ type: 1 }).then((list: any) => {
+    list.unshift({ id: 0, name: '全部' })
+    state.materialCates = list
+    const { cate } = route.query
+    cate && (state.currentIndex = cate as string)
+    cate && action('change', state.materialCates[Number(cate)], Number(cate))
+  })
 }
 
 watch(
